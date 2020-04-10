78cc7586075d5c952fbc2910603d6d6189cebc5b
Pietro Baldassarri <pietro.baldassarri@gmail.com>
2020-04-10 18:19:58 +0200

    ci(core): fix tests/common/sql/su.sql
    Fixes [error] Error thrown while running command "app:test:setup-db". Message: "mysql cli command failed"

--- tests/common/sql/su.sql
+++ tests/common/sql/su.sql
@@ -1,8 +1,13 @@
 CREATE USER 'rdig_test'@'localhost' IDENTIFIED BY '@rdigpw_Test20';
 
 GRANT ALTER, CREATE, CREATE VIEW, DROP, INDEX, REFERENCES, SHOW DATABASES, SHOW VIEW, SUPER, TRIGGER
-    ON *.* TO 'rdig_test'@'localhost'
+    ON *.*
+    TO 'rdig_test'@'localhost'
     IDENTIFIED BY '@rdigpw_Test20';
-;
+
+GRANT ALL
+    ON rdig_next_test.*
+    TO 'rdig_test'@'localhost'
+    IDENTIFIED BY '@rdigpw_Test20';
 
 FLUSH PRIVILEGES;
